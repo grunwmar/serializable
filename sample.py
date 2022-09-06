e26c88ff09f74f22b4e79243e068bb9de38129b5
Martin Grunwald <martingrunwald91@gmail.com>
2022-09-06 22:58:50 +0200

    Sample contains also example of ISerializable error when absmethos arent implemented.

--- sample.py
+++ sample.py
@@ -2,7 +2,7 @@ from serializable import ISerializable, Serializable
 import json
 
 
-class MyClass(Serializable):
+class MyClassA(Serializable):
 
     def __init__(self, **kwargs):
         for k, v in kwargs.items():
@@ -13,7 +13,7 @@ class MyClass(Serializable):
 
 
 
-my_class = MyClass()
+my_class = MyClassA()
 
 json_string = """{
     "name": "Jeroným",
@@ -22,7 +22,33 @@ json_string = """{
 }"""
 
 my_class.deserialize(json_string)
+my_class.serialize(filename="./sample_output.json", indent=3, ensure_ascii=False)
 
 print(my_class)
 
-my_class.serialize(filename="./sample_output.json", indent=3, ensure_ascii=False)
+# PRINTS:
+#
+# MyClassA(name='Jeroným', surname='Hrkal', age=31)
+#
+
+
+# example of exception when abstractmethods
+# defined in interface are not implemented
+class MyClassB(ISerializable):
+
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            self.__dict__[k] = v
+
+    def __setattr__(self, name, value):
+        super(__class__, self).__setattr__(name, value)
+
+my_class = MyClassB()
+
+# PRINTS:
+#
+# Traceback (most recent call last):
+#   File "/home/User/Python/Serializable/sample.py", line 45, in <module>
+#     my_class = MyClassB()
+#  TypeError: Can't instantiate abstract class MyClassB with abstract methods deserialize, serialize
+#
